714af3b9282acf6115864642e2c52d82e194a3fd
recepergan <68998200+recepergan@users.noreply.github.com>
2022-11-07 16:58:24 +0300

    one - question

---
+++ src/OneQuestion.java
@@ -0,0 +1,6 @@
+public class OneQuestion {
+
+
+    //Öncelikle anagram sorudada ifade edildiği gibi iki sözcüğün aynı harflerden eşit sayıda olması gerekmektedir.
+    // "Balıkçı" ve "Bıçakla" sözcüklerine baktığımız zaman "a" ve "ı" harflerinin eşit sayıda olmadığı görülmektedir.
+}
